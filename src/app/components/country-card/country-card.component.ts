c293f609b8a9d39da21d34a97d105b576d7ee0b4
bayan-404 <bayan.jubah@gmail.com>
2021-03-16 23:48:21 +0200

    add the component files and get the all countries data

--- src/app/components/country-card/country-card.component.ts
+++ src/app/components/country-card/country-card.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CountryCardService } from './country-card.service';
 @Component({
   selector: 'app-country-card',
   templateUrl: './country-card.component.html',
-  styleUrls: ['./country-card.component.css']
+  styleUrls: ['./country-card.component.css'],
 })
 export class CountryCardComponent implements OnInit {
+  CountriesData$: any = [];
 
-  constructor() { }
+  constructor(private countryCardService: CountryCardService) {}
 
   ngOnInit(): void {
+    this.CountriesData$ = this.countryCardService.getAllCountries();
   }
-
 }
