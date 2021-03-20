72cf85a16527da2f8501853fd8fcd15223d6d3a7
bayan-404 <bayan.jubah@gmail.com>
2021-03-21 01:57:19 +0200

    subsicribe to the observable and get the code from the params

--- src/app/components/country-details/country-details.component.ts
+++ src/app/components/country-details/country-details.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
+import { CountryDetailsService } from './country-details.service';
+import { Country } from '../../models/Country';
 @Component({
-  selector: 'app-country-details',
+  selector: 'country-details',
   templateUrl: './country-details.component.html',
-  styleUrls: ['./country-details.component.css']
+  styleUrls: ['./country-details.component.css'],
 })
 export class CountryDetailsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  countryName: string = '';
+  countryDetails: any = {};
+  constructor(
+    private route: ActivatedRoute,
+    private countryDetailsSevice: CountryDetailsService
+  ) {
+    this.countryDetails = {};
   }
 
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((params: any) => {
+      this.countryDetailsSevice
+        .getCountry(params.get('code'))
+        .subscribe((contryData: any) => {
+          this.countryDetails = contryData;
+        });
+    });
+  }
 }
