bd96b7600704af680d500a3db27358cf95558f44
bayan-404 <bayan.jubah@gmail.com>
2021-03-21 01:52:32 +0200

    import the  RouterModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { routsTable } from './routs';
 
 import { CountryDetailsComponent } from './components/country-details/country-details.component';
 import { HomePageComponent } from './components/home-page/home.component';
 import { FormsModule } from '@angular/forms';
-
 @NgModule({
   declarations: [AppComponent, CountryDetailsComponent, HomePageComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(routsTable),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
