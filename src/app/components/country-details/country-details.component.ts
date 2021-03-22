2767319e226e4d00196fdfcce1fe597edfd6f63d
bayan-404 <bayan.jubah@gmail.com>
2021-03-22 02:05:38 +0200

    give the country type to the data

--- src/app/components/country-details/country-details.component.ts
+++ src/app/components/country-details/country-details.component.ts
@@ -9,23 +9,23 @@ import { Country } from '../../models/Country';
 })
 export class CountryDetailsComponent implements OnInit {
   countryName: string = '';
-  countryDetails: any = {};
-  CountryBorders: any = [];
+  countryDetails: any;
+  countryBorders: Array<string>;
   isLoading: boolean = true;
   constructor(
     private route: ActivatedRoute,
     private countryDetailsSevice: CountryDetailsService
   ) {
-    this.countryDetails = {};
+    this.countryBorders = [];
   }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe((params: any) => {
       this.countryDetailsSevice
         .getCountry(params.get('code'))
-        .subscribe((contryData: any) => {
+        .subscribe((contryData: Country) => {
           this.countryDetails = contryData;
-          this.CountryBorders = this.countryDetails.borders;
+          this.countryBorders = this.countryDetails.borders;
           this.countryDetails
             ? (this.isLoading = false)
             : (this.isLoading = true);
