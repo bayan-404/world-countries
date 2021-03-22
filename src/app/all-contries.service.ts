a5a20a7be8c72e496deab69c692b195c6532a93d
bayan-404 <bayan.jubah@gmail.com>
2021-03-22 02:04:16 +0200

    add type to both requists

--- src/app/all-contries.service.ts
+++ src/app/all-contries.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root',
 })
 export class AllContriesService {
   constructor(private http: HttpClient) {}
 
-  getAllCountries() {
+  getAllCountries(): Observable<any> {
     return this.http.get('https://restcountries.eu/rest/v2/all');
   }
 }
--- src/app/components/country-details/country-details.service.ts
+++ src/app/components/country-details/country-details.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root',
 })
 export class CountryDetailsService {
   constructor(private http: HttpClient) {}
 
-  getCountry(code: string) {
+  getCountry(code: string): Observable<any> {
     return this.http.get(`https://restcountries.eu/rest/v2/alpha/${code}`);
   }
 }
